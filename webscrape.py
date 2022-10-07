d821e072fc691586876cf7011eb5e7785cc76073
Henrik Langlott <henrik.langlott@gmail.com>
2022-10-07 14:34:55 +0200

    Temperatur

---
+++ webscrape.py
@@ -0,0 +1,20 @@
+import tempfile
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://www.klart.se/se/uppsala-l%C3%A4n/v%C3%A4der-uppsala/'
+
+page = requests.get(url)
+soup = BeautifulSoup(page.text, "html.parser")
+temp = soup.find_all('div', attrs={'class': 'item weather'})
+
+
+# for span in temp:
+#     print(span)
+
+
+
+for span in temp:
+    print(span.text)
+  
