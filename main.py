b8d6206fde07f42ea133e08d28367d164bce9510
jesperbv4 <112415725+jesperbv4@users.noreply.github.com>
2022-10-12 12:56:27 +0200

    testa

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+#Skickar en get.requset och returnerar svaret
 def response(url):
     url = url
     s = requests.Session()
     r = s.get(url)
     return r
 
+#Skapar ett soup objekt från svaret av response
 def scraper(url):
     soup = BeautifulSoup(response(url).text, 'html.parser')
     return soup
 
+#Extraherar län och ort
 def data_extract(url):
     results = scraper(url).find('div', attrs={'class':'link-list'}).find_all('a', attrs={'class': 'link'})
     cities = [results[i].contents[0] for i in range(len(results)-1)]  
@@ -18,6 +21,7 @@ def data_extract(url):
     city_links = {cities[i]: links[i] for i in range(len(cities))}
     return city_links
 
+#Extraherar max och min temperatur från dagens datum och 14 dagar frammåt
 def temp_extract(url):
     results = scraper(url).find_all('div', attrs={'class': 'item weather'})
     temp_high = [int(results[i].find('span').find_all('span')[0].contents[0][0:-1]) for i in range(len(results)-1)]
@@ -25,7 +29,35 @@ def temp_extract(url):
     temp = [(temp_high[i], temp_low[i]) for i in range(len(temp_high))]
     return temp
 
+def indexify(dict):
+    y = enumerate(dict, start=1)
+    return y
+
+def print_index(dict):
+    for index, key in indexify(dict):
+        print(f"{index}) {key}")
+
+def choose_dict(dict):
+    print_index(dict)
+    while True:
+        selected = select()
+        for index, key in indexify(dict):
+            if selected == str(index) or selected.capitalize() == key:
+                return dict[key]
+
+def select():
+    return input("Option: ")
+
+def new_page(url):
+    res = (data_extract(url))
+    new_url = url + choose_dict(res)
+    return new_url
+
+def main():
+    url = 'https://www.klart.se'
+    new_page(url)
+
+
+
 if __name__ == "__main__":
-    standard_input= ''  #Behövs för ett tillägg jag använder
-    url = 'https://www.klart.se/'
-    print(data_extract(url))
+    main()
