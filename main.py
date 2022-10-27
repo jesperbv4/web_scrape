66bd3876c0c5957c4d17ae68321c434d4622565f
DownwindElm0 <henrik.langlott@gmail.com>
2022-10-27 19:34:58 +0200

    Har lagt till några kommentarer

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from class_klart import Klart, Klart_details, merge
 from tabulate import tabulate
 
+# Ger varje nyckel i en dictionary ett index.
 def indexify(dict):
     y = enumerate(dict, start=1)
     return y
 
+# Skriver ut alla index med respektive nyckel. 
 def print_index(dict):
     for index, key in indexify(dict):
         print(f"{index}) {key}")
 
+# Visar index och nycklar från en dictionary, returnerar värdet från en vald nyckel.
 def value(dict):
     print_index(dict)
     while True:
@@ -17,13 +20,16 @@ def value(dict):
             if selected == str(index) or selected.capitalize() == key:
                 return dict[key]
 
+
 def select():
     return input("Option: ")
 
+# Skriver ut elementen i en lista i en tabell.
 def print_table(a_list):
     col_names = labels()
     data = a_list
     print(tabulate(data, headers=col_names, tablefmt="fancy_grid"))
+
 
 def labels():
     labels = [
@@ -41,6 +47,7 @@ def labels():
         ]
     return labels  
 
+# Användaren får välja om den vill gå tillbaka till menu2 eller välja en ny ort.
 def menu(details):
     options = {'Gå tillbaka':'back', 'Välj ny ort':'place', 'Avsluta':'end'}
     val = value(options)
@@ -53,7 +60,7 @@ def menu(details):
             
 
         
-
+# Låter användaren välja mellan att visa datan för en specifik dag eller för 14 dagar samtidigt.
 def menu2(details):
     options2 = {'En dag': True, '14 dagar': False, 'Avsluta': 'end'}
     val_dag = value(options2)
